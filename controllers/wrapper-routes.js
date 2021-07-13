3d370659ceeb5a0ff429173160bdddc8f210483a
Old Prego <spymasterluke@gmail.com>
2021-07-13 10:58:35 -0500

    Finished the wrapper routes file, which will be used to handle all the simpler single render routes.

--- controllers/wrapper-routes.js
+++ controllers/wrapper-routes.js
@@ -1,10 +1,21 @@
 const router = require('express').Router();
-const { User, Post } = require('../models/');
+const { User, Post, Comment } = require('../models/');
+
+router.get('/signup', (req, res) => {
+    if(req.session.loggedIn) {
+        res.redirect('/');
+        return;
+    }
+
+    res.render('signup');
+})
 
 router.get('/login', (req, res) => {
-    res.render('login');
-    return
-})
+    if (req.session.loggedIn) {
+        res.redirect('/');
+        return;
+    }
+});
 
 router.get('/', async (req, res) => {
     try {
@@ -15,6 +26,30 @@ router.get('/', async (req, res) => {
         const posts = postData.map((post) => post.get({ plain: true }));
 
         res.render('posts', { posts });
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+router.get('/post/:id', async (req, res) => {
+    try {
+        const postData = await Post.findByPk(req.params.id, {
+            include: [
+                User,
+                {
+                    model: Comment,
+                    include: [User],
+                },
+            ],
+        });
+
+        if (postData) {
+            const post = postData.get({ plain: true });
+
+            res.render('single-post', { post });
+        } else {
+            res.status(404).end();
+        }
     } catch (err) {
         res.status(500).json(err);
     }
